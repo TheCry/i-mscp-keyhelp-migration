0687e17e0bba6b32c2e7170b074509198b1a0b47
S.Bay <info@space2place.de>
2020-08-08 15:34:09 +0200

    Fixed: Unknown column 'authname'

--- _keyhelp.py
+++ _keyhelp.py
@@ -658,7 +658,7 @@ class KeyHelpAddDataToServer:
                 exit(1)
         else:
             cursor = db_connection.cursor()
-            cursor.execute("INSERT INTO dir_protection (id_user, path, authname, username, password) VALUES ('" + str(
+            cursor.execute("INSERT INTO dir_protection (id_user, path, auth_name, username, password) VALUES ('" + str(
                 keyHelpData['addedKeyHelpUserId']) + "', '" + str(keyHelpData['iHtAccessPath']) + "', '" + str(
                 keyHelpData['iHtAccessAuthName']) + "', '" + str(keyHelpData['iHtAccessUserame']) + "', '" + str(
                 keyHelpData['iHtAccessPassword']) + "');")
